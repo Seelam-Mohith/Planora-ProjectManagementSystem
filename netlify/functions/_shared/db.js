3d8cc8ba940390c4c35b54ec523c6b4d725abe76
Seelam-Mohith <seelammohith2222@gmail.com>
2026-03-24 09:00:00 +0530

    set up a shared db helper for netlify functions

---
+++ netlify/functions/_shared/db.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+const connectDB = async () => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  const mongoUri = process.env.MONGO_URI;
+
+  if (!mongoUri) {
+    throw new Error("MONGO_URI is not set in the environment");
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(mongoUri).then((mongo) => mongo);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+};
+
+module.exports = connectDB;
